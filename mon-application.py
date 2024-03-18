23abbf82175094435a0fc4f9963412e055b130d2
lapinRagnar <lapinragnar@gmail.com>
2024-03-18 23:29:36 +0100

    ajouter un titre

--- mon-application.py
+++ mon-application.py
@@ -17,5 +17,13 @@ tickerData = yf.Ticker(tickerSymbol)
 tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2020-5-31')
 # Open	High	Low	Close	Volume	Dividends	Stock Splits
 
+st.write("""
+## Closing Price
+""")
 st.line_chart(tickerDf.Close)
+
+st.write("""
+## Volume
+""")
+
 st.line_chart(tickerDf.Volume)
